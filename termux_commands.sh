8841be11b580286cdfecea0165a93b9153be6a5a
James Scott McDowell <mcdowellj87@gmail.com>
2023-02-01 22:03:53 -0500

    Update termux_commands.sh

--- termux_commands.sh
+++ termux_commands.sh
@@ -5,3 +5,7 @@ cp /sdcard/Download/rezzer.py /data/data/com.termux/files/home/Source
 cd Source
 python -m venv venv
 . ./venv/bin/activate
+
+export FLASK_APP=rezzer.py
+export FLASK_ENV=""
+export FLASK_DEBUG=""
